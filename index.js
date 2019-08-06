9d620c52d4fb447d12dae03d87a36be6259c10cd
Jason Santana <jsantana006@gmail.com>
2019-08-06 23:23:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var recipes = {};
 
 
 function updateObjectWithKeyAndValue(object, key, value){
-  var newObject = object;
-  //newObject[key] = value;
+  var newObject = {object};
+  newObject[key] = value;
   return newObject;
 }
 

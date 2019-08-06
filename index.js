0e17c8da9094d9b483b4145735419cfe689b718f
Jason Santana <jsantana006@gmail.com>
2019-08-06 23:15:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,22 +1,6 @@
 var recipes = {};
 
 
-function updateObjectWithKeyAndValue(object, key, value){
-  var newObject = Object.assign({}, object);
-  return newObject;
-}
 
-
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  object[key] = value;
-  return object;
-}
-
-
-function deleteFromObjectByKey(object, key){
-  var nObject = Object.assign({}, object);
-  delete nObject.key;
-  return nObject;
-}
 
 

dbfa8156a8ac54617b80e4b5fc23af3f61eddd7a
Jason Santana <jsantana006@gmail.com>
2019-08-06 23:14:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 
 function deleteFromObjectByKey(object, key){
   var nObject = Object.assign({}, object);
-  delete nObject.`key`;
+  delete nObject.key;
   return nObject;
 }
 
